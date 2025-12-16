2a31d15c030a705026d66517544a2f6fdd0d480a
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-12-16 20:34:59 +0100

    Add commits-per-month chart on the repo page

--- src/digitavlen/pages/repo.clj
+++ src/digitavlen/pages/repo.clj
@@ -1,5 +1,24 @@
 (ns digitavlen.pages.repo
-  (:require [superstring.core :as str]))
+  (:require [digitavlen.aggregation :as aggregation]
+            [mattilsynet.design :as mtds]
+            [superstring.core :as str]))
 
 (defn render [db page]
-  [:h1 (str/capitalize (:repo/name page))])
+  (let [commits (aggregation/commits-in db page)]
+    [:main {:class (mtds/classes :prose :group)}
+     [:h1 (str/capitalize (:repo/name page))]
+
+     (let [dataene (aggregation/commits-per-month commits)]
+       [:mtds-chart {:class (mtds/classes :card)
+                     :style {:--mtdsc-chart-aspect "4 / 1"}}
+        [:table
+         [:thead
+          [:tr
+           [:th]
+           (for [[måned _] dataene]
+             [:th måned])]]
+         [:tbody
+          [:tr
+           [:th "Commits per måned"]
+           (for [[_ cnt] dataene]
+             [:td cnt])]]]])]))
