abab313b662b6ec5bf402dc5d5516c0d5a951de1
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-12-18 09:36:56 +0100

    Add charts to render-year, render-month and render-week
    - Year is rendered by week
    - Month is rendered by day
    - Week is rendered by half day

--- src/digitavlen/pages/repo.clj
+++ src/digitavlen/pages/repo.clj
@@ -1,6 +1,9 @@
 (ns digitavlen.pages.repo
-  (:require [digitavlen.aggregate :as aggregate]
+  (:require [cljc.java-time.local-date :as ld]
+            [cljc.java-time.year-month :as ym]
+            [digitavlen.aggregate :as aggregate]
             [digitavlen.navigation :as navigation]
+            [digitavlen.time :as time]
             [mattilsynet.design :as mtds]
             [superstring.core :as str]))
 
@@ -28,22 +31,97 @@
              [:td cnt])]]]])]))
 
 (defn render-year [db page]
-  (let [repo (:git/repo page)]
+  (let [repo (:git/repo page)
+        commits (filter (partial aggregate/by-year (:param/year page))
+                        (aggregate/commits-in db repo))]
     [:main {:class (mtds/classes :prose :group)}
      [:h1 (str/capitalize (:repo/name repo))]
 
-     (navigation/bar db page)]))
+     (navigation/bar db page)
+
+     (let [data (aggregate/commits-per-week commits)]
+       [:mtds-chart {:class (mtds/classes :card)
+                     :style {:--mtdsc-chart-aspect "4 / 1"}}
+        [:table
+         [:thead
+          [:tr
+           [:th]
+           (for [[week _] data]
+             [:th (second week)])]]
+         [:tbody
+          [:tr
+           [:th "Commits per week"]
+           (for [[_ cnt] data]
+             [:td cnt])]]]])]))
 
 (defn render-month [db page]
-  (let [repo (:git/repo page)]
+  (let [repo (:git/repo page)
+        commits (filter (partial aggregate/by-month
+                                 (ym/of (:param/year page) (:param/month page)))
+                        (aggregate/commits-in db repo))]
     [:main {:class (mtds/classes :prose :group)}
      [:h1 (str/capitalize (:repo/name repo))]
 
-     (navigation/bar db page)]))
+     (navigation/bar db page)
+
+     (let [data (aggregate/commits-per-day commits)]
+       [:mtds-chart {:class (mtds/classes :card)
+                     :style {:--mtdsc-chart-aspect "4 / 1"}}
+        [:table
+         [:thead
+          [:tr
+           [:th]
+           (for [[ld _] data]
+             [:th (ld/get-day-of-month ld)])]]
+         [:tbody
+          [:tr
+           [:th "Commits per day"]
+           (for [[_ cnt] data]
+             [:td cnt])]]]])]))
 
 (defn render-week [db page]
-  (let [repo (:git/repo page)]
+  (let [repo (:git/repo page)
+        commits (filter (partial aggregate/by-week
+                                 [(:param/year page) (:param/week page)])
+                        (aggregate/commits-in db repo))]
     [:main {:class (mtds/classes :prose :group)}
      [:h1 (str/capitalize (:repo/name repo))]
 
-     (navigation/bar db page)]))
+     (navigation/bar db page)
+
+     (let [data (->> (aggregate/commits-per-half-day commits)
+                     (group-by ffirst))]
+       [:mtds-chart {:class (mtds/classes :card)
+                     :style {:--mtdsc-chart-aspect "4 / 1"}}
+        [:table
+         [:thead
+          [:tr
+           [:th]
+           (for [[ld _] data]
+             [:th (str (time/->month (ld/get-month-value ld)) " "
+                       (ld/get-day-of-month ld))])]]
+         [:tbody
+          [:tr
+           [:th "Commits per morning"]
+           (for [[_ cnt] data]
+             [:td (-> cnt first second)])]
+          [:tr
+           [:th "Commits per afternoon"]
+           (for [[_ cnt] data]
+             [:td (-> cnt second second)])]]]])]))
+
+(comment
+
+  (do
+    (require '[powerpack.dev :as dev]
+             '[datomic.api :as d])
+    (def app (dev/get-app))
+    (def db (d/db (:datomic/conn app))))
+
+  (def week-11-commits (filter (partial aggregate/by-week [2025 11])
+                               (aggregate/commits-in db {:repo/id "mattilsynet/matnyttig"})))
+
+  (->> (aggregate/commits-per-half-day week-11-commits)
+       (group-by ffirst))
+
+  )
