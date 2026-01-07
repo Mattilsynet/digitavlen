b0d2b4a0f7208b63609fdf0db6ffb24a514c443d
Mathias Iversen <mathias.iversen@mattilsynet.no>
2026-01-07 08:27:03 +0100

    Try outputting git result temporarily

--- src/unpack/git.clj
+++ src/unpack/git.clj
@@ -63,6 +63,7 @@
   (let [log (get-git-log! {:repo-path repo-path
                            :separator "%n"
                            :with-numstat true})]
+    (println "GITLOGOUTPUT\n" log)
     (->> (str/split log #"\n")
          (reduce partitionize [])
          (mapv process-commit))))
