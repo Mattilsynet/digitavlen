10872dddcdf9506ae8d8c6945e38f0a9d2939e36
Mathias Iversen <mathias.iversen@mattilsynet.no>
2026-01-07 09:19:34 +0100

    Check output on git log command

--- src/unpack/git.clj
+++ src/unpack/git.clj
@@ -32,10 +32,9 @@
   )
 
 (defn get-git-log! [opts]
-  (let [{:keys [repo-path with-numstat separator pretty-format]} (merge default-options opts)]
-    (->> (build-git-log-command repo-path with-numstat separator pretty-format)
-         (apply p/sh)
-         :out)))
+  (let [{:keys [repo-path with-numstat separator pretty-format]} (merge default-options opts)
+        cmd (build-git-log-command repo-path with-numstat separator pretty-format)]
+    (:out (p/check (apply p/sh cmd)))))
 
 (defn matches-git-hash [s]
   (re-matches #"^[a-f0-9]{40}.*" s))
