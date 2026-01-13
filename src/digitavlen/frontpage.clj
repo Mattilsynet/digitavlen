37d09a23f84135a279e058c57b90f379a71a4f52
Mathias Iversen <mathias.iversen@mattilsynet.no>
2026-01-13 12:21:13 +0100

    Let the repo-list unfold horizontally to save space

--- src/digitavlen/frontpage.clj
+++ src/digitavlen/frontpage.clj
@@ -6,12 +6,13 @@
 (defn render [db _]
   [:main {:class (mtds/classes :prose :group)}
    [:h1 "Digitavlen"]
-   (for [repo (->> (d/q '[:find [?e ...]
-                          :where
-                          [?e :page/kind :page.kind/repo]]
-                        db)
-                   (db/entities db)
-                   (sort-by :page/uri))]
-     [:div
-      [:a {:href (:page/uri repo)}
-       (:repo/display-name (:git/repo repo))]])])
+   [:div {:class (mtds/classes :flex)}
+    (for [repo (->> (d/q '[:find [?e ...]
+                           :where
+                           [?e :page/kind :page.kind/repo]]
+                         db)
+                    (db/entities db)
+                    (sort-by :page/uri))]
+      [:div
+       [:a {:href (:page/uri repo)}
+        (:repo/display-name (:git/repo repo))]])]])
