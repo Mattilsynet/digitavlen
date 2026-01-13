14c88003d77feeeb8e13afd35eaced93439cfdad
Mathias Iversen <mathias.iversen@mattilsynet.no>
2026-01-13 12:20:27 +0100

    Add db queries to get commits and authors

--- src/digitavlen/db.clj
+++ src/digitavlen/db.clj
@@ -4,6 +4,13 @@
 (defn entities [db entity-ids]
   (map #(d/entity db %) entity-ids))
 
+(defn get-commits [db]
+  (->> (d/q '[:find [?c ...]
+              :where
+              [?c :commit/hash]]
+            db)
+       (entities db)))
+
 (defn commits-in [db repo]
   (->> (d/q '[:find [?c ...]
               :in $ ?repo-id
@@ -11,5 +18,21 @@
               [?r :repo/id ?repo-id]
               [?c :git/repo ?r]
               [?c :commit/hash]]
+            db (:repo/id repo))
+       (entities db)))
+
+(defn get-authors [db]
+  (->> (d/q '[:find [?p ...]
+              :where [?p :person/email]]
+            db)
+       (entities db)))
+
+(defn get-repo-authors [db repo]
+  (->> (d/q '[:find [?author ...]
+              :in $ ?repo-id
+              :where
+              [?r :repo/id ?repo-id]
+              [?c :git/repo ?r]
+              [?c :commit/author ?author]]
             db (:repo/id repo))
        (entities db)))
